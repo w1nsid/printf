83bda60070806e5d1b456c46941536492d00642c
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-04-25 18:22:41 +0100

    betty

--- _printf.c
+++ _printf.c
@@ -1,12 +1,5 @@
 #include "main.h"
 
-/**
- * _printf - Produces output according to a format.
- * @format: A character string containing zero or more directives.
- *
- * Return: The number of characters printed (excluding the null byte used to
- * end output to strings), or -1 on failure.
- */
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -24,41 +17,26 @@ int _printf(const char *format, ...)
 			{
 			case 'c':
 				ch = (char)va_arg(args, int);
-				_putchar(ch);
-				printed_chars++;
+				_putchar(ch), printed_chars++;
 				break;
-
 			case 's':
 				str_arg = va_arg(args, char *);
 				if (str_arg == NULL)
 					str_arg = "(null)";
 				while (*str_arg)
-				{
-					_putchar(*str_arg);
-					str_arg++;
-					printed_chars++;
-				}
+					_putchar(*str_arg), str_arg++, printed_chars++;
 				break;
-
 			case '%':
-				write(1, "%", 1);
-				printed_chars++;
+				_putchar('%'), printed_chars++;
 				break;
-
 			default:
 				return (-1);
 			}
 		}
 		else
-		{
-			_putchar(*format);
-			printed_chars++;
-		}
-
+			_putchar(*format), printed_chars++;
 		format++;
 	}
-
 	va_end(args);
-
 	return (printed_chars);
 }
